3b841c82d6b4bc2e89f5d2c2f01638d4c3c6f454
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-04-23 15:49:50 +0100

    added new component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
-const Pet = ({name, animal, breed})  =>  {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, name),
-        React.createElement('h2', {}, animal),
-        React.createElement('h2', {}, breed)
-    ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
+
 const App = () =>   {
          return React.createElement(
              "div",
@@ -22,5 +19,5 @@ const App = () =>   {
                     })
             ])   
         };
-        ReactDOM.render(React.createElement(App),
+        render(React.createElement(App),
          document.getElementById('root'));
