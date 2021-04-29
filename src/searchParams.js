63b197bb34ee4b740458dc3d5572ed2c31632445
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-04-29 12:09:56 +0100

    added animalapi from front-end masters

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () =>  {
-    const locaton = "ikoyi, Lag"
+    const [locaton,setlocation] = useState('Ikoyi LAG')
+    const [animal, setAnimal] = useState('dog');
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" placeholder="location" value={locaton} />
+                    <input id="location" placeholder="location" value={locaton} onChange ={e => setlocation(e.target.value)}/>
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select value = {animal} 
+                    id="animal"
+                    onChange ={e => setAnimal(e.target.value)}
+                    onBlur ={e => setAnimal(e.target.value)}>
+                        <option >All</option>
+                        {ANIMALS.map(animal => <option value ={animal} >{animal}</option>)}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
