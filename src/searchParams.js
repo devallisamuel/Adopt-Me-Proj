35784e271f5a9f39b1f75ff70190e4598a416100
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-05-02 15:48:20 +0100

    worked on hooks again

--- src/searchParams.js
+++ src/searchParams.js
@@ -6,15 +6,15 @@ const SearchParams = () =>  {
     const [locaton,setlocation] = useState('Ikoyi LAG')
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed","", breeds );
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed","", breeds );
     useEffect(() => {
         setBreeds([]);
         setBreeds("");
-        pet.breeds(animal).then(({ beeeds }) =>{
-            const breedStrings = breeds.map(({ name }) => name);
+        pet.breeds(animal).then(({ beeeds:apiBreeds }) =>{
+            const breedStrings = apiBreeds.map(({ name }) => name);
             setBreeds(breedStrings);
         }, console.error);
-    });
+    }, [animal, setBreed, setBreeds]);
     return (
         <div className="search-params">
             <form>
