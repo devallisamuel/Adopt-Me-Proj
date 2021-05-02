125885b375d50e49a5aae63d589713a8b5a58126
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-05-02 01:40:12 +0100

    worked on hooks

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import { useDropdown } from "./useDropdown";
 
 const SearchParams = () =>  {
     const [locaton,setlocation] = useState('Ikoyi LAG')
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed","", breeds )
+    const [breed, BreedDropdown] = useDropdown("Breed","", breeds );
+    useEffect(() => {
+        setBreeds([]);
+        setBreeds("");
+        pet.breeds(animal).then(({ beeeds }) =>{
+            const breedStrings = breeds.map(({ name }) => name);
+            setBreeds(breedStrings);
+        }, console.error);
+    });
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" placeholder="location" value={locaton} onChange ={e => setlocation(e.target.value)}/>
+                    <input id="location" placeholder="location" value={locaton} 
+                    onChange ={e => setlocation(e.target.value)}/>
                 </label>
                <AnimalDropdown />
                <BreedDropdown />
